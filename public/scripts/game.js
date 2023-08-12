02acd6e7f17d298f2e62b35611fdbc3004cf93bd
Wet Takis <77040285+wtstudios@users.noreply.github.com>
2023-08-12 13:37:44 -0700

    Update game.js

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -24,7 +24,7 @@ let socket,
   sourceSansPro,
   ping;
 
-socket = io.connect(window.location.origin);
+socket = io.connect('rekoil-hp-usw.onrender.com');
 
 function keyReleased() {
   if(assetsAreLoaded) {
