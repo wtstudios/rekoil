36f2b04f8b73f742cc79f7dff98cae66e83940ce
Wet Takis <77040285+wtstudios@users.noreply.github.com>
2023-10-05 20:51:29 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -333,12 +333,12 @@ function updatePlayer(player, delay) {
   if(w || s) {
     Body.setVelocity(body, {
       x: body.velocity.x,
-      y: +(w ^ s) && (((a ^ d) ? 0.7071 : 1) * [-1, 1][+w] * base * delay / (body.density / 0.15))
+      y: +(w ^ s) && (((a ^ d) ? 0.7071 : 1) * [-1, 1][+w] * base * delay / 1.5 / (body.density / 0.15))
     });
   }
   if(a || d) {
     Body.setVelocity(body, {
-      x: +(a ^ d) && (((w ^ s) ? 0.7071 : 1) * [-1, 1][+d] * base * delay / (body.density / 0.15)),
+      x: +(a ^ d) && (((w ^ s) ? 0.7071 : 1) * [-1, 1][+d] * base * delay / 1.5 / (body.density / 0.15)),
       y: body.velocity.y
     });
   }
